38f67e09fc30a7a263081ebbc9596a8fe4829eb7
minsunny5 <minsunny5@gmail.com>
2022-09-07 02:01:55 +0900

    Practice 2,3. Draw 2 Triangles with 2 VAO,VBO and 2 fragment shader

--- Start.cpp
+++ Start.cpp
@@ -20,7 +20,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 1.5f, 0.2f, 1.0f);\n"//주황색
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"//주황색
 "}\n\0";
 const char* fragmentShaderSource_yellow = "#version 330 core\n"
 "out vec4 FragColor;\n"
@@ -66,11 +66,8 @@ int main()
 	/* 버텍스 셰이더 오브젝트 만들기 */
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);//vertexShader -> 이 ID에 VertexShader를 붙이겠다!
-	//셰이더 코드 붙여주기
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);	//셰이더 코드 붙여주기
 	glCompileShader(vertexShader);
-
-	//컴파일 잘됐나 확인하기
 	int success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);//컴파일 성공했는지 확인
@@ -85,8 +82,6 @@ int main()
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);//셰이더 오브젝트 생성
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);//오브젝트에 셰이더 코드 붙여주기
 	glCompileShader(fragmentShader);
-
-	//컴파일 잘됐나 확인하기
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);//컴파일 성공했는지 확인
 	if (!success)
 	{
@@ -94,13 +89,11 @@ int main()
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
-	/* 프래그먼트 셰이더 오브젝트 만들기 */
+	/* 노란색 프래그먼트 셰이더 오브젝트 만들기 */
 	unsigned int fragmentShader_yellow;
 	fragmentShader_yellow = glCreateShader(GL_FRAGMENT_SHADER);//셰이더 오브젝트 생성
-	glShaderSource(fragmentShader_yellow, 1, &fragmentShaderSource, NULL);//오브젝트에 셰이더 코드 붙여주기
+	glShaderSource(fragmentShader_yellow, 1, &fragmentShaderSource_yellow, NULL);//오브젝트에 셰이더 코드 붙여주기
 	glCompileShader(fragmentShader_yellow);
-
-	//컴파일 잘됐나 확인하기
 	glGetShaderiv(fragmentShader_yellow, GL_COMPILE_STATUS, &success);//컴파일 성공했는지 확인
 	if (!success)
 	{
@@ -138,9 +131,10 @@ int main()
 	}
 	
 
-	//이제 셰이더 오브젝트들은 필요없으니 해제
+	//링크 끝났으면 이제 셰이더 오브젝트들은 필요없으니 해제
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader_yellow);
 
 
 	//ViewPort setting (1st,2nd) arguments means lower left corner of the window. (-1 to 1) 
@@ -149,47 +143,38 @@ int main()
 	// ------------------------------------------------------------------
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	float vertices[] = {
+	float vertices1[] = {
 		// first triangle
 		-1.0f, -0.5f, 0.0f,  // left 
 		 0.0f, -0.5f, 0.0f,  // right
 		-0.5f, 0.5f, 0.0f,  // top 
+	};
+	float vertices2[] = {
 		// second triangle
 		 0.0f, -0.5f, 0.0f,  // left
-		 0.1f, -0.5f, 0.0f,  // right
+		 1.0f, -0.5f, 0.0f,  // right
 		 0.5f, 0.5f, 0.0f   // top 
 	};
 	
 	/* 버텍스 버퍼 오브젝트 만들기 */
 	//openGL의 다른 오브젝트들 처럼 VBO도 오브젝트니까 고유 ID를 받아야한다. -> glGenBuffer로 겟
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	unsigned int VBOs[2], VAOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
 
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-
-
-	//3.vertex attribute을 설정하자.
-	//vertex data를 이렇게 해석해라 하고 알려주는 부분
-	//1번 인수:우리가 vertex shader에서 layout (location = 0) 로 aPos라는 변수를 선언했었는데 그 변수에 값을 연결해주는 과정이다.
-	//2번 인수:버퍼에 들어있는 값중 3개를 0번 변수에 넣어주라는 뜻
-	//3번 인수:정규화 하기 vs 안하기
-	//4번 인수:stride 다음 vertex 데이터는 딱 3칸(float * 3) 있다가 나온다고 알려주는 것, tightly pack되어있다면 0넣으면 openGL이 알아서 값을 넣는다.
-	//5번 인수: 버퍼에서 얼마만큼 떨어진 상태가 되어야 vertex 데이터가 시작되는지 offset
+	//첫번째 삼각형 세팅
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	//ㄴ이 함수를 쓰면 자동으로 GL_ARRAY_BUFFER로 바운드 되어있는 VBO에 할당되어있는 메모리에 접근해서 vertex attribute 0번에 vertex data를 연결한다.
 	glEnableVertexAttribArray(0);
-	//이제 VBO와 Vertex Attribute가 설정되었다고 알려준다.
 
-	//glVertexAttribPointer()함수를 통해서 이미 VBO랑 연결이 되었으니 GL_ARRAY_BUFFER와 바인딩을 풀어놔도 된다.
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// 다른 VAO call이 이 VAO를 건드릴까봐 unbind 할 수 있지만 거의 안일어나는 일이긴 한다.
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	glBindVertexArray(0);
+	//두번째 삼각형 세팅
+	glBindVertexArray(VAOs[1]);//glBindVertexArray에 다른걸 바인딩하면 건드릴 VAO대상이 바뀜.
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	// uncomment this call to draw in wireframe polygons.
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -204,21 +189,24 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//버퍼 리셋할 때는 이 컬러로 바꾼다.(state-setting function)
 		glClear(GL_COLOR_BUFFER_BIT);//버퍼를 리셋한다.(state-using function) GL_DEPTH_BUFFER_BIT,GL_STENCIL_BUFFER_BIT도 CLEAR가능
 
-		//사각형 그리기
-		glUseProgram(shaderProgram_yellow);//만든 프로그램 활성화 시키기
-		glBindVertexArray(VAO);//솔직히 지금 그리는 오브젝트가 하나밖에 없는데 이걸 계속 바인딩해줄 필요는 없지만 통일성을 위해 그냥 해준다.
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		//2번째 인자: indices가 6개라서
-		//glBindVertexArray(0); //매번 unbind 해줄 필요는 없다.
+		//삼각형 2개 그리기
+		glUseProgram(shaderProgram);//처음에 만든 프로그램 활성화 시키기
+		glBindVertexArray(VAOs[0]);//먼저 첫번째 VAO를 그리겠다고 세팅해줌.
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		glUseProgram(shaderProgram_yellow);//yellow 버전 프로그램 활성화 시키기
+		glBindVertexArray(VAOs[1]);//이제 두번째 VAO를 그리겠다고 세팅해줌.
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);//백버퍼를 모니터에 출력
 		glfwPollEvents();//트리거된 이벤트(키보드 인풋이나 마우스 인풋 등)가 있는지 확인
 	}
 
 	//메모리 해제
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
 	glDeleteProgram(shaderProgram);
+	glDeleteProgram(shaderProgram_yellow);
 	//glfw 리소스 메모리 해제
 	glfwTerminate();
 
