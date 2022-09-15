b75fd3ab89c5a7026d5f16b3fc00de11ae3b6a61
minsunny5 <minsunny5@gmail.com>
2022-09-15 21:48:10 +0900

    [Texture-1,2] Make Face Upside-down by fragment shader and Compare Wrapping Modes

--- texture.fs
+++ texture.fs
@@ -9,5 +9,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord),texture(texture2, TexCoord),0.2); //텍스쳐 coord와 텍스쳐를 받아와서 색을 샘플링한다
+    FragColor = mix(texture(texture1, TexCoord),texture(texture2, vec2(TexCoord.x,-TexCoord.y)), 0.2);
 }
--- texture.vs
+++ texture.vs
@@ -8,7 +8,7 @@ out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(0.0, 1.0, 0.0, 0.0) - vec4(aPos, 1.0);
+    gl_Position = vec4(aPos, 1.0);
     ourColor = aColor;
     TexCoord = aTexCoord;
 }
