a7f36ad767c43f8e6bcfed98d6b81b9cab746ff0
minsunny5 <minsunny5@gmail.com>
2022-09-22 20:21:44 +0900

    [BasicLighting-3] Phong Shading in View Space

--- color.fs
+++ color.fs
@@ -3,11 +3,10 @@ out vec4 FragColor;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec3 LightPos;//view space로 변환된 광원의 위치벡터
   
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
 
 void main()
 {
@@ -17,17 +16,17 @@ void main()
 
     //diffuse lighting
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(LightPos - FragPos);
 
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     //specular lighting
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos);//원래 viewPos - FragPos인데 View space라 view pos는 항상 (0,0,0)이다.
     vec3 reflectDir = reflect(-lightDir, norm);  
 
-    float spec = pow(max(dot(viewDir, reflectDir),0.0), 256);
+    float spec = pow(max(dot(viewDir, reflectDir),0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
